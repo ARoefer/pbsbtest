7c4181c722b8b1858f8bcfaa4e58a9975fd57fdd
Adrian Röfer <aroefer@uni-bremen.de>
2018-06-14 01:35:05 +0200

    UR5 force wipe test and sensor readouts

--- src/pbsbtest/force_sensor_test.py
+++ src/pbsbtest/force_sensor_test.py
@@ -5,7 +5,7 @@ import sys
 from time import time
 from gebsyas.ros_visualizer import ROSVisualizer
 from giskardpy.robot import Robot
-from giskardpy.symengine_wrappers import point3, pos_of, norm, x_col, y_col, z_col, unitZ, dot, frame3_rpy, abs
+from giskardpy.symengine_wrappers import point3, vec3, pos_of, norm, x_col, y_col, z_col, unitZ, dot, frame3_rpy, abs, pi
 from pbsbtest.utils import res_pkg_path
 from sensor_msgs.msg import JointState as JointStateMsg
 from std_msgs.msg import Empty as EmptyMsg
@@ -16,6 +16,7 @@ from giskardpy.qpcontroller import QPController
 from giskardpy.qp_problem_builder import SoftConstraint
 from giskardpy.input_system import ControllerInputArray, ScalarInput
 from giskardpy.input_system import FrameInputRPY as FrameInput
+from geometry_msgs.msg import WrenchStamped as WrenchMsg
 
 from iai_bullet_sim.basic_simulator import BasicSimulator
 from iai_bullet_sim.ros_modules import JSPublisher, SensorPublisher
@@ -43,7 +44,7 @@ class EEFPositionControl(QPController):
         wrist_frame = robot.frames[self.wrist]
         eef_frame = robot.frames[self.eef]
         eef_y = y_col(eef_frame)
-        eef_forward = x_col(eef_frame)
+        eef_forward = z_col(eef_frame)
 
 
         goal_expr = self.goal_eef[self.eef].get_expression()
@@ -53,7 +54,7 @@ class EEFPositionControl(QPController):
         
         x_z_dot = dot(eef_forward, -goal_z)
         x_z_dot_ctrl = 1 - x_z_dot
-        y_x_dot = dot(eef_y, goal_x)
+        y_x_dot = abs(dot(eef_y, goal_x))
         y_x_dot_ctrl = 1 - y_x_dot
         # dist = norm(sp.Add(pos_of(eef_frame), - goal_expr, evaluate=False))
         eef_in_goal = pos_of(wrist_frame) - pos_of(goal_expr)
@@ -61,8 +62,6 @@ class EEFPositionControl(QPController):
         dist_y = abs(dot(goal_y, eef_in_goal))
         dist_z = abs(dot(goal_z, eef_in_goal))
         #dist_ctrl = x_z_dot * y_x_dot * -min(dist, self.velocity)
-        
-
         
         self._soft_constraints['align {} position_x'.format(self.eef)] = SoftConstraint(lower=-dist_x,
                                                                         upper=-dist_x,
@@ -90,6 +89,7 @@ class EEFPositionControl(QPController):
         self.update_observables({self.goal_weights[self.eef].get_symbol_str(): self.weight})
         self.set_goal([0]*6)
 
+
     def set_goal(self, goal):
         """
         dict eef_name -> goal_position
@@ -102,31 +102,40 @@ class EEFPositionControl(QPController):
 
 class Node(object):
     def __init__(self, urdf_path, base_link, eef_frame, wrist_link, wps):
+        self.vis = ROSVisualizer('force_test', base_frame=base_link, plot_topic='plot')
         self.wps = wps
         self.wpsIdx = 0
-        
-        self.vis = ROSVisualizer('force_test', base_frame=base_link, plot_topic='plot')
 
+        # Giskard
         self.robot = Robot()
         self.robot.load_from_urdf_path(res_pkg_path(urdf_path), base_link, [eef_frame])
-        self.wrist_pos = pos_of(self.robot.frames[wrist_link])
+        self.sensor_frame = self.robot.frames[wrist_link]
+        self.wrist_pos = pos_of(self.sensor_frame)
+
+
+        mplate_pos = pos_of(self.robot.frames['arm_mounting_plate'])
+        self.motion_frame = frame3_rpy(pi, 0, pi * 0.5, mplate_pos)
+        self.sensor_motion_frame_inv = self.motion_frame.inv() * self.sensor_frame
 
         self.controller = EEFPositionControl(self.robot, eef_frame, wrist_link, velocity=1)
-        self.controller.set_goal(self.wps[self.wpsIdx])
+        waypoint = self.motion_frame * point3(*(self.wps[self.wpsIdx][3:]))
+        self.controller.set_goal((0,0,0, waypoint[0], waypoint[1], waypoint[2]))
 
+
+        # Sim things
         self.tick_rate = 50
         self.sim = BasicSimulator(self.tick_rate)
         self.sim.init(mode='gui')
     
         plane = self.sim.load_urdf('package://iai_bullet_sim/urdf/plane.urdf', useFixedBase=1)
 
-        self.ur5 = self.sim.load_urdf('package://iai_table_robot_description/robots/ur5_table.urdf', useFixedBase=1)
-        self.ur5.enable_joint_sensor('wrist_3_joint')
+        self.sensor_joint = 'wrist_3_joint'
+        self.ur5 = self.sim.load_urdf(urdf_path, useFixedBase=1)
+        self.ur5.enable_joint_sensor(self.sensor_joint)
         ur5Id = self.sim.get_body_id(self.ur5.bId())
         self.ur5_js_pub = JSPublisher(self.ur5, ur5Id)
-        self.ur5_sensor_pub = SensorPublisher(self.ur5, ur5Id)
         self.sim.register_post_physics_cb(self.ur5_js_pub.update)
-        self.sim.register_post_physics_cb(self.ur5_sensor_pub.update)
+        self.sensor_pub = rospy.Publisher('{}/sensors/{}'.format(ur5Id, self.sensor_joint), WrenchMsg, queue_size=1)
         
         self.clock_pub = rospy.Publisher('/clock', ClockMsg, queue_size=1)
         self.time = rospy.Time()
@@ -134,6 +143,9 @@ class Node(object):
         self.terminal = Terminal()
         self.__last_tick = None
         self.__last_msg_len = 0
+
+        self.ur5_fixed_positions = {#'gripper_base_gripper_left_joint': 0,
+                                    'gripper_joint': 0}
 
 
     def tick(self):
@@ -145,21 +157,39 @@ class Node(object):
         js = {j: s.position for j, s in self.ur5.joint_state().items()}
         self.robot.set_joint_state(js)
         eef_position = self.wrist_pos.subs(js)
-        dist = norm(eef_position - point3(*(self.wps[self.wpsIdx][3:])))        
+        waypoint = self.motion_frame * point3(*(self.wps[self.wpsIdx][3:]))
+        dist = norm(eef_position - waypoint)        
         if dist < 0.03:
             self.wpsIdx = (self.wpsIdx + 1) % len(wps)
             print('\nWaypoint reaced. Next one is number {}'.format(self.wpsIdx))
-            self.controller.set_goal(self.wps[self.wpsIdx])
+        
+        self.controller.set_goal((0,0,0, waypoint[0], waypoint[1], waypoint[2]))
 
         next_cmd = self.controller.get_next_command()
 
+
         self.ur5.apply_joint_vel_cmds(next_cmd)
+        self.ur5.apply_joint_pos_cmds(self.ur5_fixed_positions)
 
         self.sim.update()
 
+        state = self.ur5.get_sensor_states()['wrist_3_joint']
+        f = self.sensor_motion_frame_inv.subs(js) * vec3(*state.f)
+        t = self.sensor_motion_frame_inv.subs(js) * vec3(*state.m) 
+
+        sensor_msg = WrenchMsg()
+        sensor_msg.header.stamp = rospy.Time.now()
+        sensor_msg.wrench.force.x  = f[0] 
+        sensor_msg.wrench.force.y  = f[1] 
+        sensor_msg.wrench.force.z  = f[2] 
+        sensor_msg.wrench.torque.x = t[0] 
+        sensor_msg.wrench.torque.y = t[1] 
+        sensor_msg.wrench.torque.z = t[2] 
+        self.sensor_pub.publish(sensor_msg)
+
         self.vis.begin_draw_cycle()
         self.vis.draw_sphere('eef_giskard', eef_position, 0.025)
-        self.vis.draw_sphere('goal', self.wps[self.wpsIdx][3:], 0.025, r=0, g=1)
+        self.vis.draw_sphere('goal', waypoint, 0.025, r=0, g=1)
         self.vis.render()
 
         now = time()
@@ -179,17 +209,15 @@ if __name__ == '__main__':
     
     urdf_path = 'package://iai_table_robot_description/robots/ur5_table.urdf'
 
-    wrist_link = 'wrist_3_link'
-    eef_frame = 'ee_link'#
+    wrist_link = 'ee_link'
+    eef_frame = 'gripper_tool_frame'#
     base_link = 'map'
 
 
-    on_table_height = 0.9
-
-    wps = [(0,0,0, -0.3,-0.4,on_table_height + 0.1),
-           (0,0,0, -0.3,-0.4,on_table_height), 
-           (0,0,0, -0.3,-0.2,on_table_height),
-           (0,0,0, -0.3,-0.2,on_table_height + 0.1)] #[point3(1,1,1.1), point3(1,1,1), point3(0.2,0,1), point3(0.2,0,1.1)]
+    wps = [(0,0,0, -0.1, 0.5, -0.1 - 0.213),
+           (0,0,0, -0.1, 0.5, -0.213), 
+           (0,0,0,  0.1, 0.5, -0.213),
+           (0,0,0,  0.1, 0.5, -0.1 - 0.213)] #[point3(1,1,1.1), point3(1,1,1), point3(0.2,0,1), point3(0.2,0,1.1)]
 
     node = Node(urdf_path, base_link, eef_frame, wrist_link, wps)
 
